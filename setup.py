0258ccb6f55223ec9a5a378f1aeb84dc1a1ee1d6
Mikael Nordin <mikael.nordin@su.se>
2020-11-26 17:45:32 +0100

    v0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wireguide",
-    version="0.0.4",
+    version="0.0.5",
     author="Mikael Nordin",
     author_email="mik@elnord.in",
     description="A WireGuard GUI for GNU/Linux",
