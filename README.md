17095aa076a51792cdee916dc433b06bfb936e67
Mikael Nordin <mikael.nordin@su.se>
2020-11-26 16:28:51 +0100

    Packaging for pip

--- README.md
+++ README.md
@@ -14,12 +14,9 @@ For packaging a debian package you can use these dependencies:
 Thanks to Jan Bodnar of zetcode.com for the valuable tutorial on wxPython dialogs:
 * http://zetcode.com/wxpython/dialogs/
 
-## Usage
+## Installation
 ```
-sudo apt get install libnm0 wxpython gir1.2-nm-1.0
-git clone https://github.com/mickenordin/wireguide.git
-cd wireguide/
-./wireguide
+pip install wireguide
 ```
 ## Screenshots
 ![No config](https://raw.githubusercontent.com/mickenordin/wireguide/main/screenshots/scrot0.png)
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wireguide",
+    version="0.0.1",
+    author="Mikael Nordin",
+    author_email="mik@elnord.in",
+    description="A WireGuard GUI for GNU/Linux",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mickenordin/wireguide",
+    packages=setuptools.find_packages(),
+    scripts=["wireguide/wireguide"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Environment :: X11 Applications",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
