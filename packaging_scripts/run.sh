28fc6be3ef45057b0da4842b66e585a2c1665816
Micke Nordin <mik@elnord.in>
2020-12-18 21:11:53 +0100

    removing shasums

--- packaging_scripts/run.sh
+++ packaging_scripts/run.sh
@@ -3,4 +3,3 @@ BASEDIR=~/sources
 ${BASEDIR}/wireguide/pakaging_scripts/version.sh
 ${BASEDIR}/wireguide/pakaging_scripts/reprepro.sh
 ${BASEDIR}/wireguide/pakaging_scripts/alien.sh
-${BASEDIR}/wireguide/pakaging_scripts/shasums.sh
--- packaging_scripts/shasums.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-BASEDIR=~/sources
-sha256sum ${BASEDIR}/wireguide{-,_}*|sed "s_  ${HOME}/sources/_|_" | wl-copy
-vim ${BASEDIR}/wireguide/README.md
