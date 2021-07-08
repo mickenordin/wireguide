446fc57afaa70fb8de9921629e23ea0b230dbef1
Micke Nordin <kano@sunet.se>
2021-07-08 11:20:44 +0200

    Moving to smolnet.org

--- packaging_scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-BASEDIR=~/sources
-${BASEDIR}/wireguide/packaging_scripts/version.sh
-${BASEDIR}/wireguide/packaging_scripts/reprepro.sh
-${BASEDIR}/wireguide/packaging_scripts/alien.sh
